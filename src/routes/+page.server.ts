c5f030ecb921a057e7bfc9e6a1b1fdda7db73ae9
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-02-14 12:44:53 +0530

    done '/' route migration

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,6 +1,15 @@
 import { db } from '@tools/db';
 
 export const load = async () => {
-  const data = await db.selectFrom('rent_data').selectAll().orderBy('date desc').execute(); // getting pre sorted data
+  const data_query = db
+    .selectFrom('rent_data')
+    .selectAll()
+    .orderBy('month desc')
+    .orderBy('date desc')
+    .execute(); // getting pre sorted data
+  // sorting with month first and date then will make it easy for us to display data later on
+  // as we won't need to sort in then
+
+  const [data] = await Promise.all([data_query]);
   return { rent_data: data };
 };
