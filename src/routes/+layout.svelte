a37798f26fbc68cc5df5d4eefeffe299704d4d91
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-03-23 17:35:28 +0000

    dark theme snippet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,17 @@
-<script>
+<script lang="ts">
   import '../app.pcss';
 </script>
 
 <div class="container mx-auto">
   <slot />
 </div>
+<svelte:head>
+  <script>
+    if (
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    )
+      document.documentElement.classList.add('dark');
+    else document.documentElement.classList.remove('dark');
+  </script>
+</svelte:head>
