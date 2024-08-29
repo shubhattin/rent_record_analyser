6dac01a34946199bceb02d148548a96fa48012fc
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-08-29 17:07:46 +0530

    init tanstack query

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
   import { ModeWatcher } from 'mode-watcher';
   import { computePosition, autoUpdate, offset, shift, flip, arrow } from '@floating-ui/dom';
+  import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
   import { storePopup } from '@skeletonlabs/skeleton';
   import '@fontsource/roboto/latin.css';
   import '../app.pcss';
 
   // set up the floating UI for popups
   storePopup.set({ computePosition, autoUpdate, offset, shift, flip, arrow });
+
+  const queryClient = new QueryClient();
 </script>
 
 <ModeWatcher />
 <div class="contaiiner mx-auto mb-1 max-w-screen-lg">
-  <slot />
+  <QueryClientProvider client={queryClient}>
+    <slot />
+  </QueryClientProvider>
 </div>
