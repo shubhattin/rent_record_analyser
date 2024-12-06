571e0edf2c8421f4ffd0b61360a5bf600c2fd383
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2024-12-06 09:28:03 +0530

    fix

--- src/routes/list/+page.svelte
+++ src/routes/list/+page.svelte
@@ -40,13 +40,6 @@
     backdropClasses="backdrop-blur-sm"
     initialFocusEl={() => pass_input_elmnt!}
   >
-    {#snippet trigger()}
-      {#if !pass_enterer_status}
-        <button transition:slide class="fixed bottom-2 right-2 cursor-default text-3xl">
-          <Icon src={FiEdit3} class="hover:text-blue-500 active:text-red-500" />
-        </button>
-      {/if}
-    {/snippet}
     {#snippet content()}
       <AuthenticatePassword
         is_verified={false}
@@ -66,3 +59,12 @@
 <div class="my-8">
   <Edit all_data={data} {editable} />
 </div>
+{#if !pass_enterer_status}
+  <button
+    transition:slide
+    class="fixed bottom-2 right-2 cursor-default text-3xl"
+    onclick={() => (pass_enterer_status = true)}
+  >
+    <Icon src={FiEdit3} class="hover:text-blue-500 active:text-red-500" />
+  </button>
+{/if}
