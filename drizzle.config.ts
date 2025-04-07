a380d9010138e4ad32ec7d92b817d19678a4211e
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-04-07 07:19:48 +0530

    Drizzle Migrate (#24)
    * init migrate
    
    * Refactor database migration and schema files
    
    - Removed the migration script located at src/db/migrate.ts.
    - Updated migration metadata files (0000_snapshot.json, 0001_snapshot.json, 0002_snapshot.json, 0003_snapshot.json) to reflect changes in versioning and dialect from 'pg' to 'postgresql'.
    - Added new properties such as 'policies', 'isRLSEnabled', and 'checkConstraints' to the table definitions in the migration snapshots.
    - Adjusted table names to include the 'public' schema in migration metadata.
    - Updated the _journal.json file to include the new migration version.
    - Refactored schema definitions in schema.ts to simplify the syntax and improve readability.
    - Ensured consistency in the use of enums and primary key definitions across tables.
    
    * Update @neondatabase/serverless dependency to version 1.0.0

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,13 +2,13 @@ import type { Config } from 'drizzle-kit';
 import { get_db_url } from './src/db/db_utils';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: './.env.local' });
+dotenv.config({ path: './.env' });
 
 export default {
   schema: './src/db/schema.ts',
   out: './src/db/migrations',
-  driver: 'pg',
+  dialect: 'postgresql',
   dbCredentials: {
-    connectionString: get_db_url(process.env)
+    url: get_db_url(process.env)
   }
 } satisfies Config;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -16,35 +16,32 @@ export const rentTypeEnum = pgEnum('rent_type', ['rent', 'electricity']);
 export const rent_data = pgTable(
   'rent_data',
   {
-    id: serial('id').primaryKey(),
-    amount: integer('amount').notNull(),
-    date: date('date', { mode: 'date' }).notNull(),
-    month: date('month', { mode: 'date' }).notNull(),
-    user_id: integer('user_id').references(() => users.id, { onDelete: 'set null' }),
-    rent_type: rentTypeEnum('rent_type').default('rent').notNull()
+    id: serial().primaryKey(),
+    amount: integer().notNull(),
+    date: date({ mode: 'date' }).notNull(),
+    month: date({ mode: 'date' }).notNull(),
+    user_id: integer().references(() => users.id, { onDelete: 'set null' }),
+    rent_type: rentTypeEnum().default('rent').notNull()
   },
-  (table) => ({
-    dateIdx: index().on(table.date),
-    monthIdx: index().on(table.month)
-  })
+  ({ date, month }) => [index().on(date), index().on(month)]
 );
 
 export const others = pgTable('others', {
-  key: varchar('key', { length: 20 }).primaryKey(),
-  value: text('value').notNull()
+  key: varchar({ length: 20 }).primaryKey(),
+  value: text().notNull()
 });
 
 export const userTypeEnum = pgEnum('user_type', ['admin', 'non-admin']);
 
 export const users = pgTable('users', {
-  id: serial('id').primaryKey(),
-  name: varchar('name', { length: 15 }).notNull(),
-  password: char('password', { length: 96 }).notNull(), // SHA-256 hash + salt of length 32
-  user_type: userTypeEnum('user_type').default('non-admin').notNull()
+  id: serial().primaryKey(),
+  name: varchar({ length: 15 }).notNull(),
+  password: char({ length: 96 }).notNull(), // SHA-256 hash + salt of length 32
+  user_type: userTypeEnum().default('non-admin').notNull()
 });
 
 export const verification_requests = pgTable('verification_requests', {
-  id: integer('id')
+  id: integer()
     .primaryKey()
     .references(() => rent_data.id, { onDelete: 'cascade' })
 });
