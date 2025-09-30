f6b972ff25bc452a529f8b03527110e967c06928
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-09-30 11:45:07 +0530

    comment tanstack query

--- src/app/ConvexClientProvider.tsx
+++ src/app/ConvexClientProvider.tsx
@@ -4,9 +4,9 @@ import { ReactNode } from 'react';
 import { ConvexReactClient } from 'convex/react';
 import { authClient } from '@/lib/auth-client';
 import { ConvexBetterAuthProvider } from '@convex-dev/better-auth/react';
-import { queryClient } from '~/state/queryClient';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { queryClient } from '~/sstate/queryClient';
+// import { QueryClientProvider } from '@tanstack/react-query';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
   // Optionally pause queries until the user is authenticated
@@ -15,11 +15,11 @@ const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!, {
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ConvexBetterAuthProvider client={convex} authClient={authClient}>
-        {children}
-      </ConvexBetterAuthProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    // <QueryClientProvider client={queryClient}>
+    <ConvexBetterAuthProvider client={convex} authClient={authClient}>
+      {children}
+    </ConvexBetterAuthProvider>
+    // <ReactQueryDevtools initialIsOpen={false} />
+    // </QueryClientProvider>
   );
 }
