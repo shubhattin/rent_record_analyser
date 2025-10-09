6074ca919640f12457e46b8e6aa2172f6d2db668
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2025-10-09 23:27:30 +0530

    redirect

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,10 +1,13 @@
 import { api } from '$convex/_generated/api';
-import { preloadQuery } from 'convex/nextjs';
+import { fetchQuery, preloadQuery } from 'convex/nextjs';
 import { getToken } from '~/lib/auth-server';
 import List from './List';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
   const token = await getToken();
+  const user = fetchQuery(api.auth.getCurrentUser, {}, { token });
+  if (!user) redirect('/');
   const preloadedRecords = await preloadQuery(api.routes.rentData.getRentData, {}, { token });
   return <List preloadedRecords={preloadedRecords} />;
 }
